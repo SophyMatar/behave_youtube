5bac17b0e48a92a05c3a57630343871455678fe5
Sophia Matar <90429123+SophyMatar@users.noreply.github.com>
2023-03-24 12:46:51 -0400

    Update video_searching_steps.py

--- video_searching_steps.py
+++ video_searching_steps.py
@@ -17,13 +17,10 @@ def step_impl(context, site):
 @when('I enter "{search_term}" into the search bar')
 def step_impl(context, search_term):
     search_input = context.driver.find_element(By.NAME, "search_query")
+    search_input.clear()
     search_input.send_keys(search_term)
-
-@when('I click the search button')
-def step_impl(context):
-    search_button = context.driver.find_element(By.ID, "search-icon-legacy")
-    search_button.click()
-    time.sleep(5) 
+    search_input.send_keys(Keys.RETURN)
+    time.sleep(5)
 
 @then('I should see a list of videos related to "{search_term}"')
 def step_impl(context, search_term):
@@ -35,4 +32,5 @@ def step_impl(context, search_term):
     wait = WebDriverWait(context.driver, 10)
     no_results_message = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "div#container yt-icon")))
     assert f"No results found for {search_term}" in no_results_message.get_attribute("aria-label")
-
+    
+#Had to look up how to do this, but the rest I was able to base on the amazon behave example given in class.
